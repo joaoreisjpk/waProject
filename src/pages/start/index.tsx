a7a573324fcde0fca07ebe727b057123a4f82721
João Reis <joaoreisjpk@gmail.com>
2021-12-12 23:41:09 -0300

    terminando a validaçao e a implementaçao do formik

--- src/pages/start/index.tsx
+++ src/pages/start/index.tsx
@@ -1,7 +1,6 @@
 import { Button, Link, Typography } from '@mui/material';
 import NextLink from 'next/link';
 import Header from '../../components/Header';
-import { useQuestions } from '../../context/useQuestions';
 
 import SendIcon from '@mui/icons-material/Send';
 import CancelIcon from '@mui/icons-material/Cancel';
@@ -10,25 +9,21 @@ export default function Start() {
   return (
     <>
       <Header />
-      <Button
-        endIcon={<SendIcon />}
-        size='large'
-        variant='contained'
-      >
-        <NextLink href='/questions'>
-          <Link color='secondary' underline='none'>
+      <NextLink href='/questions'>
+        <Link color='secondary' underline='none'>
+          <Button endIcon={<SendIcon />} size='large' variant='contained'>
             <Typography variant='h5'>Start</Typography>
-          </Link>
-        </NextLink>
-      </Button>
+          </Button>
+        </Link>
+      </NextLink>
 
-      <Button endIcon={<CancelIcon />} size='large' variant='contained'>
-        <NextLink href='/'>
-          <Link href='#' color='secondary' underline='none'>
+      <NextLink href='/'>
+        <Link href='#' color='secondary' underline='none'>
+          <Button endIcon={<CancelIcon />} size='large' variant='contained'>
             <Typography variant='h5'>Cancel</Typography>
-          </Link>
-        </NextLink>
-      </Button>
+          </Button>
+        </Link>
+      </NextLink>
     </>
   );
 }
